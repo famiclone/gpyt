d0cc30d751d2dfd492270358ba44dd97f089c683
Denys Tynok <denys@famiclone.dev>
2023-06-14 11:02:40 +0300

    refactor: add ChatEngine class

--- src/main.py
+++ src/main.py
@@ -1,24 +1,64 @@
 import openai
 import os
 from pygments import highlight
-from pygments.lexers import PythonLexer, RustLexer
+from pygments.lexers import PythonLexer, RustLexer, guess_lexer
 from pygments.formatters import TerminalFormatter
 
-def get_completion(prompt, model="gpt-3.5-turbo"):
-    messages = [{"role": "user", "content": prompt}]
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=0,  # this is the degree of randomness of the model's output
-    )
-    return response.choices[0].message["content"]
+# TODO: add class for API calls
+# TODO: add class for history management
+# TODO: add sqlite database for storing history
+# TODO: add class for formatting output
+
+
+class CodeFormatter:
+    def __init__(self):
+        pass
+
+    def get_language(self, text: str) -> str:
+        pass
+
+    def get_lexer(self, text: str):
+        pass
+
+    def format(self, text: str) -> str:
+        pass
+
+class HistoryManager:
+    def __init__(self):
+        pass
+
+    def add(self, text: str):
+        pass
+
+    def get(self, text: str):
+        pass
+
+    def clear(self):
+        pass
+
+class ChatEngine:
+    def __init__(self, api_key: str):
+        self.model = "gpt-3.5-turbo"
+        self.history = ""
+        self.temperature = 0.9
+        self.engine = openai
+        self.engine.api_key = api_key
+
+    def get_completion(self, prompt: str) -> str:
+        messages = [{"role": "user", "content": prompt}]
+        response = openai.ChatCompletion.create(
+            model=self.model,
+            messages=messages,
+            temperature=self.temperature,  # this is the degree of randomness of the model's output
+        )
+        return response.choices[0].message["content"]
 
 
 def main():
     from dotenv import load_dotenv, find_dotenv
     _ = load_dotenv(find_dotenv())
 
-    openai.api_key = os.getenv('OPENAI_API_KEY')
+    chat = ChatEngine(os.getenv('OPENAI_API_KEY'))
     run = True
     # clear Terminal
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -28,7 +68,7 @@ def main():
         if prompt == "exit":
             run = False
         else:
-            response = get_completion(prompt)
+            response = chat.get_completion(prompt)
             # if response has a code block, highlight only the code block
             if "```" in response:
                 # get language of code block
@@ -38,13 +78,13 @@ def main():
                 if lang == "rust":
                     lexer = RustLexer()
 
-
                 response = response.split("```")
                 response = response[0] + "```" + highlight(response[1], lexer, TerminalFormatter()) + "```"
 
             print(response)
             print("-------------------")
             print("")
+
 
 if __name__ == '__main__':
     main()
